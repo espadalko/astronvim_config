a6d4b54d204f7ef563064b425a0fb4634503e195
espadalko <espadalko@me.com>
2023-11-17 12:10:40 +0700

    noswap and plugin color tailwind

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,13 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end
+  },
 }
