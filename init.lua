062206a53da0f1ebb1a8531beed42a0cd6b8120f
espadalko <espadalko@me.com>
2023-11-17 13:27:58 +0700

    fix plugin tailwind

--- init.lua
+++ init.lua
@@ -65,10 +65,14 @@ return {
     },
   },
 
+
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    require("cmp").setup({ 
+      formatting = { format = require("tailwindcss-colorizer-cmp").formatter } 
+    })
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
